4bcaa9304905a1c2f47eb33dcb76041569200520
caubinpj <caubinpj@madrid.es>
2025-08-26 13:33:58 +0200

    boton de recarga

--- visor.js
+++ visor.js
@@ -44,18 +44,78 @@ function prepare_box_data(temp_data) {
     return data
 }
 
+async function load_data() {
+    const url = "https://ciudadesabiertas.madrid.es/dynamicAPI/API/query/meteo_tiemporeal.json?pageSize=5000"; // reemplaza por tu URL
+    const data = await loadJson(url);
+    console.log("JSON cargado en memoria:", data);
+    const response = data;
+    let norm_data = normalize_data(response.records);
+    console.log("datos normalizados", norm_data);
+    const temp_data = norm_data.filter(item => item.MAGNITUD === "83" && item.VALIDEZ === "V");
+    console.log("datos temperatura", temp_data);
+    let plot_data = prepare_box_data(temp_data);
+    console.log("datos plot", plot_data);
+    return plot_data;
+}
+
+function plot_temp_data(plot_data, hora, div) {
+    const layout = {
+        title: { text: `Temperaturas [ ${hora} ]` },
+        legend: { visible: false },
+        xaxis: { autorangeoptions: { maxallowed: 24 }, dtick: 1 },
+        font: { size: 12 },
+        plot_bgcolor: 'rgba(250,250, 250, 0.5)',
+        paper_bgcolor: 'rgba(250,250, 250, 0.5)',
+        font: {
+            family: 'verdana, arial, sans-serif',
+            size: 12,
+            color: 'rgb(100,150,200)'
+        },
+    };
+    const plot_conf = { responsive: true }
+    Plotly.newPlot(div, plot_data, layout, plot_conf);
+}
+
+async function load_show() {
+    try {
+        const spinner = document.getElementById("spinner");
+        const tester = document.getElementById('tester');
+        spinner.style.display = "block"; // Mostrar spinner
+        tester.style.display = "none"; //oculta el grafico
+        const plot_data = await load_data()
+        const ahora = new Date();
+        const hora = ahora.toLocaleTimeString();
+        plot_temp_data(plot_data, ahora, tester );
+        tester.style.display = "block";
+    } catch (error) {
+        const errorItem = document.createElement("li");
+        errorItem.textContent = "Error al cargar los datos: " + error.message;
+        errorItem.style.color = "red";
+        lista.appendChild(errorItem);
+    } finally {
+        spinner.style.display = "none"; // Ocultar spinner
+    }
+
+}
+
+document.getElementById("cargarBtn").addEventListener("click", load_show);
+
+/*
+
 (async () => {
     try {
-        const url = "https://ciudadesabiertas.madrid.es/dynamicAPI/API/query/meteo_tiemporeal.json?pageSize=5000"; // reemplaza por tu URL
-        const data = await loadJson(url);
-        console.log("JSON cargado en memoria:", data);
-        response = data;
-        let norm_data = normalize_data(response.records);
-        console.log("datos normalizados", norm_data);
-        temp_data = norm_data.filter(item => item.MAGNITUD === "83" && item.VALIDEZ === "V");
-        console.log("datos temperatura", temp_data);
-        let plot_data = prepare_box_data(temp_data);
-        console.log("datos plot", plot_data);
+         const url = "https://ciudadesabiertas.madrid.es/dynamicAPI/API/query/meteo_tiemporeal.json?pageSize=5000"; // reemplaza por tu URL
+         const data = await loadJson(url);
+         console.log("JSON cargado en memoria:", data);
+         response = data;
+         let norm_data = normalize_data(response.records);
+         console.log("datos normalizados", norm_data);
+         temp_data = norm_data.filter(item => item.MAGNITUD === "83" && item.VALIDEZ === "V");
+         console.log("datos temperatura", temp_data);
+         let plot_data = prepare_box_data(temp_data);
+         console.log("datos plot", plot_data);
+
+        plot_data = await load_data()
 
         const layout = {
             title: { text: 'Temperaturas de hoy' },
@@ -70,16 +130,15 @@ function prepare_box_data(temp_data) {
                 color: 'rgb(100,150,200)'
             },
         };
-
         const plot_conf = { responsive: true }
-
         TESTER = document.getElementById('tester');
         Plotly.newPlot(TESTER, plot_data, layout, plot_conf);
+        plot_temp_data(plot_data)
 
     } catch (err) {
         console.error(err);
     }
-})();
+})();*/
 
 
 
